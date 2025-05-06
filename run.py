a7a7874762ecd112d21363b8a0550dc367e2a316
Kerem-Haeger <mvtf3siy@students.codeinstitute.net>
2025-05-06 09:47:03 +0100

    Set up a worksheet for a leaderboard later

--- run.py
+++ run.py
@@ -10,7 +10,25 @@
 # import time
 import re
 import random
+import gspread
+from google.oauth2.service_account import Credentials
 from events import Predator
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('leaderboard')
+
+board = SHEET.worksheet('highscores')
+data = board.get_all_values()
+# print(data)
 
 
 class Species:
